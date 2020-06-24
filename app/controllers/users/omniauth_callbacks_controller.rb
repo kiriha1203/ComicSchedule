4b69b11bf260db3aff06d3f217fb85f7179d854e
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-06-24 22:15:36 +0900

    fix omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,15 +6,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   #common callback method
   def callback_for(provider)
-    @omniauth = request.env['omniauth.auth']
-    info = User.find_oauth(@omniauth)
-    @user = info[:user]
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      @sns = info[:sns]
-      render template: "devise/registrations/new"
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
     end
   end
 
